9b5830019dbd366b6c021744e54e2c3a8d20472d
Jeferson Gomes <67127121+jeferson3@users.noreply.github.com>
2021-12-21 11:02:52 -0300

    finalizando controller de usuário

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -10,7 +10,15 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Retorna todos os usuários do banco
+     *
+     * @OA\Get (
+     *     path="/users",
+     *     summary="UserController",
+     *     @OA\Response(response="200", description="Resposta com sucesso"),
+     *     tags={"Controllers - Usuários"},
+     *     security={{ "apiAuth": {} }}
+     * )
      *
      * @return JsonResponse
      */
@@ -20,7 +28,26 @@ class UserController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Salva um novo usuário no banco
+     *
+     * @OA\Post (
+     *     path="/users",
+     *     summary="UserController",
+     *     @OA\Response(response="200", description="Resposta com sucesso"),
+     *     @OA\RequestBody(
+     *          required=true,
+     *          description="Salvar usuário",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="name", type="String", example="admin"),
+     *              @OA\Property(property="email", type="String", example="admin@email.com"),
+     *              @OA\Property(property="password", type="String", example="password"),
+     *              @OA\Property(property="company_id", type="Int", example=1),
+     *          )
+     *     ),
+     *
+     *     tags={"Controllers - Usuários"},
+     *     security={{ "apiAuth": {} }}
+     * )
      *
      * @param  Request  $request
      * @return JsonResponse
@@ -28,11 +55,10 @@ class UserController extends Controller
     public function store(Request $request)
     {
 
-        $name  = $request->get('name');
-        $email = $request->get('email');
-        $pass  = $request->get('password');
+        $name        = $request->get('name');
+        $email       = $request->get('email');
+        $pass        = $request->get('password');
         $company_id  = $request->get('company_id');
-
 
         if (!is_null($name) && !is_null($email) && !is_null($pass) && !is_null($company_id)) {
 
@@ -56,14 +82,34 @@ class UserController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * Retorna um usuário específico pelo ID
      *
-     * @param  User  $user
+     * @OA\Get (
+     *     path="/users/{id}",
+     *     summary="UserController",
+     *     @OA\Response(response="200", description="Resposta com sucesso"),
+     *     @OA\Response(response="404", description="Resposta com erro"),
+     *
+     *     @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          required=true,
+     *          description="ID do usuário",
+     *          @OA\Schema(
+     *              type="string"
+     *          ),
+     *     ),
+     *
+     *     tags={"Controllers - Usuários"},
+     *     security={{ "apiAuth": {} }}
+     * )
+     *
+     * @param  int $id
      * @return JsonResponse
      */
-    public function show(User $user)
+    public function show(int $id): JsonResponse
     {
-        if (is_null($user) || empty($user)) {
+        if (!$user = User::find($id)) {
             return response()->json([
                 'status'    => false,
                 'message'   => 'Nenhum registro encontrado!'
@@ -74,16 +120,46 @@ class UserController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * Atualiza um usuário no banco
+     *
+     *  @OA\Put (
+     *     path="/users/{id}",
+     *     summary="UserController",
+     *     @OA\Response(response="200", description="Resposta com sucesso"),
+     *     @OA\Response(response="404", description="Resposta com erro"),
+     *
+     *     @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          required=true,
+     *          description="ID do usuário",
+     *          @OA\Schema(
+     *              type="string"
+     *          ),
+     *     ),
+     *
+     *     @OA\RequestBody(
+     *          required=true,
+     *          description="Salvar usuário",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="name", type="String", example="admin"),
+     *              @OA\Property(property="email", type="String", example="admin@email.com"),
+     *              @OA\Property(property="password", type="String", example="password"),
+     *          )
+     *     ),
+     *
+     *     tags={"Controllers - Usuários"},
+     *     security={{ "apiAuth": {} }}
+     * )
      *
      * @param  Request  $request
-     * @param  User  $user
+     * @param  int $id
      * @return JsonResponse
      */
-    public function update(Request $request, User $user)
+    public function update(Request $request, int $id)
     {
 
-        if (is_null($user) || empty($user)) {
+        if (!$user = User::find($id)) {
             return response()->json([
                 'status'    => false,
                 'message'   => 'Nenhum registro encontrado!'
@@ -93,15 +169,13 @@ class UserController extends Controller
         $name       = $request->get('name');
         $email      = $request->get('email');
         $pass       = $request->get('password');
-        $company_id = $request->get('company_id');
 
-        if (!is_null($name) && !is_null($email) && !is_null($pass) && !is_null($company_id)) {
+        if (!is_null($name) && !is_null($email) && !is_null($pass)) {
 
             $user->update([
                 'name' => $request->get('name'),
                 'email' => $request->get('email'),
-                'password' => $request->get('password'),
-                'company_id' => $company_id,
+                'password' => $request->get('password')
             ]);
 
             return response()->json([
@@ -117,14 +191,33 @@ class UserController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Deleta um usuário do banco
      *
-     * @param  User  $user
+     * @OA\Delete  (
+     *     path="/users/{id}",
+     *     summary="UserController",
+     *     @OA\Response(response="200", description="Resposta com sucesso"),
+     *     @OA\Response(response="404", description="Resposta com erro"),
+     *
+     *     @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          required=true,
+     *          description="ID do usuário",
+     *          @OA\Schema(
+     *              type="string"
+     *          ),
+     *     ),
+     *
+     *     tags={"Controllers - Usuários"},
+     *     security={{ "apiAuth": {} }}
+     * )
+     * @param  int $id
      * @return JsonResponse
      */
-    public function destroy(User $user)
+    public function destroy(int $id): JsonResponse
     {
-        if (is_null($user) || empty($user)) {
+        if (!$user = User::find($id)) {
             return response()->json([
                 'status'    => false,
                 'message'   => 'Nenhum registro encontrado!'
