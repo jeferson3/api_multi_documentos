7169c166051174380c2fba5963b7acfab0767255
Jeferson Gomes <67127121+jeferson3@users.noreply.github.com>
2021-12-21 11:03:05 -0300

    finalizando controller de autenticação

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,61 +4,103 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
 
     public function __construct()
     {
-        $this->middleware('authApi', ['except' => 'login']);
+        $this->middleware('authApi', ['except' => ['login']]);
     }
 
     /**
-     * Get a JWT via given credentials.
+     * Faz o login e retorna o token JWT
+     *
+     * @OA\Post  (
+     *     path="/auth/login",
+     *     summary="AuthController",
+     *     @OA\Response(response="400", description="Resposta com erro"),
+     *     @OA\Response(response="200", description="Resposta com sucesso"),
+     *
+     *     @OA\RequestBody(
+     *          required=true,
+     *          description="Fazer login",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="email", type="String", example="admin@email.com"),
+     *              @OA\Property(property="password", type="String", example="password"),
+     *          )
+     *     ),
+     *
+     *     tags={"Controllers - Autenticação"}
+     * )
      *
      * @return JsonResponse
      */
-    public function login()
+    public function login(): JsonResponse
     {
         $credentials = request(['email', 'password']);
 
-        if (!$token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+        if (!$token = auth()->attempt($credentials, true)) {
+            return response()->json(['status' => false, 'message' => 'Credenciais inválidas!'], 400);
         }
 
-        return response()->json(['token' => $token]);
+        return response()->json(['token' => $token])->setStatusCode(200);
     }
 
     /**
-     * Get the authenticated User.
+     * Retorna o usuário logado
+     *
+     * @OA\Post  (
+     *     path="/auth/me",
+     *     summary="AuthController",
+     *     @OA\Response(response="200", description="Resposta com sucesso"),
+     *     security={{ "apiAuth": {} }},
+     *
+     *     tags={"Controllers - Autenticação"}
+     *    )
      *
      * @return JsonResponse
      */
-    public function me()
+    public function me(): JsonResponse
     {
         return response()->json(auth()->user());
     }
 
     /**
-     * Log the user out (Invalidate the token).
+     * Faz o logout do usuário invalidando o token
+     *
+     * @OA\Post  (
+     *     path="/auth/logout",
+     *     summary="AuthController",
+     *     @OA\Response(response="200", description="Resposta com sucesso"),
+     *     security={{ "apiAuth": {} }},
+     *
+     *     tags={"Controllers - Autenticação"}
+     *    )
      *
      * @return JsonResponse
      */
-    public function logout()
+    public function logout(): JsonResponse
     {
         auth()->logout();
-
-        return response()->json(['message' => 'Successfully logged out']);
+        return response()->json(['status' => true, 'message' => 'Logout realizado com sucesso']);
     }
 
     /**
-     * Refresh a token.
+     * Refresh do token.
      *
+     * @OA\Post  (
+     *     path="/auth/refresh",
+     *     summary="AuthController",
+     *     @OA\Response(response="200", description="Resposta com sucesso"),
+     *     security={{ "apiAuth": {} }},
+     *
+     *     tags={"Controllers - Autenticação"}
+     *    )
      * @return JsonResponse
      */
-    public function refresh()
+    public function refresh(): JsonResponse
     {
-        return $this->respondWithToken(auth()->refresh());
+        return response()->json(['token' => auth()->refresh()])->setStatusCode(200);
     }
 }
