c7a1f5d25715e61f5940ebecdfc180d52aa7e335
Jeferson Gomes <67127121+jeferson3@users.noreply.github.com>
2021-12-21 21:33:58 -0300

    permissions

--- routes/api.php
+++ routes/api.php
@@ -8,16 +8,11 @@ Route::group(['prefix' => '/auth'], function () {
     Route::post('/logout', [\App\Http\Controllers\Auth\AuthController::class, 'logout']);
     Route::post('/refresh', [\App\Http\Controllers\Auth\AuthController::class, 'refresh']);
     Route::post('/me', [\App\Http\Controllers\Auth\AuthController::class, 'me']);
+    Route::post('/permissions', [\App\Http\Controllers\Auth\AuthController::class, 'permissions']);
 });
 
 Route::group(['prefix' => '/', 'middleware' => 'authApi'], function () {
     Route::apiResource('/companies', \App\Http\Controllers\Company\CompanyController::class);
     Route::apiResource('/users', \App\Http\Controllers\User\UserController::class);
     Route::apiResource('/documents', \App\Http\Controllers\Document\DocumentController::class);
-
-    Route::group(['prefix' => 'permissions', 'middleware' => 'admin'], function (){
-        Route::get('/', [\App\Http\Controllers\Permission\PermissionController::class, 'index']);
-        Route::post('/setPermission', [\App\Http\Controllers\Permission\PermissionController::class, 'setPermission']);
-        Route::post('/removePermission', [\App\Http\Controllers\Permission\PermissionController::class, 'removePermission']);
-    });
 });
